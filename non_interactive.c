25b550fa38ce5ea2c8d624f8161cd8178183d342
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-16 19:18:39 +0300

    update non_interactive.c with betty style

--- non_interactive.c
+++ non_interactive.c
@@ -8,30 +8,29 @@
  * @pipe: its take 1 but with non interactive mode we hange it to 0
  * Return: alwayd int
 */
-
 void non_interactive(int argc, char *argv[], char *env[], int *pipe)
 {
 	int bytes, err_count = 1;
 	const char *del = " ";
-	size_t n_buffer = 0;	
+	size_t n_buffer = 0;
 	char *buffer = NULL, command[50], *args[20], *only_command;
 	*pipe = 0;
 	while ((bytes = getline(&buffer, &n_buffer, stdin)) != -1)
 	{
 		trim_buffer(buffer);
-		if(_strcmp(buffer, "exit") == 0)
+		if (_strcmp(buffer, "exit") == 0)
 			free(buffer), exit(0);
 		only_command = take_only_cmd(buffer);
-		if(*buffer)
+		if (*buffer)
 		{
-			_tokenize(buffer, args, del, command, env);	
+			_tokenize(buffer, args, del, command, env);
 			if (access(command, X_OK) == 0)
 				_fork(argc, argv, buffer, args, only_command);
-			else	
+			else
 				_printf("%s: %d: %s: not found\n",
 						argv[argc - 1], err_count++, only_command);
 		}
-		free(only_command);	
+		free(only_command);
 	}
 	free(buffer);
 }
