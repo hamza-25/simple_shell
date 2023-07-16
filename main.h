797620bf68703edd1359be2c05495cc8b9d48c39
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-16 15:22:19 +0300

    update main.h with betty style

--- main.h
+++ main.h
@@ -22,14 +22,16 @@ size_t _strlen(const char *str);
 void _print(const char *str);
 char *_strdup(const char *str);
 void _fork(int argc, char *argv[], char *buf, char **ave, char *only);
-int _strcmp(const char* string1, const char* string2);
-void _tokenize(char *buf, char *args[], const char *del, char *str, char *env[]);
-char *full_path(char *env[],char cmd[]);
-void _printf(const char* format, ...);
+int _strcmp(const char *string1, const char *string2);
+void _tokenize(char *buf, char *args[], const char *del,
+							char *str, char *env[]);
+char *full_path(char *env[], char cmd[]);
+void _printf(const char *format, ...);
 void tostring(int num);
 char *path_check(char path[]);
 void _env(char *env[]);
-void non_interactive (int argc, char *argv[], char *env[], int *pipe);
+void non_interactive(int argc, char *argv[], char *env[], int *pipe);
 char *take_only_cmd(char *buffer);
 ssize_t _getline(char **line, size_t *n, FILE *stream);
+
 #endif
