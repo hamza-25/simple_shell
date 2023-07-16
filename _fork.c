75008659de317b5597b8b01ed719b889e0bb2bf0
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-16 19:12:18 +0300

    update _fork.c with betty style but need refactoring

--- _fork.c
+++ _fork.c
@@ -7,8 +7,8 @@
 * @argv: The argument vector
 * @buf: The command buffer
 * @ave: The argument vector for execve
+* @only: The command to execute
 */
-
 void _fork(int argc, char *argv[], char *buf, char *ave[], char *only)
 {
 	signed int pid;
@@ -25,18 +25,19 @@ void _fork(int argc, char *argv[], char *buf, char *ave[], char *only)
 	}
 	if (pid == 0)
 	{
-		if (_strcmp(only, "ls") == 0 || _strcmp(ave[0], "/usr/bin/ls") == 0 || _strcmp(ave[0], "/bin/ls") == 0)
+		if (_strcmp(only, "ls") == 0 || _strcmp(ave[0], "/usr/bin/ls") == 0
+				|| _strcmp(ave[0], "/bin/ls") == 0)
 		{
-			while(ave[index])
+			while (ave[index])
 			{
 				if (ave[index][0] != '-')
 				{
 					dir_path = ave[index];
 					dir = opendir(dir_path);
 					if (!dir)
-               			 	{
-                        			_printf("%s: cannot access '%s': No such file or directory\n", only, dir_path);
-                        			if (ave[index + 1] == NULL)
+					{
+						_printf("%s: cannot access '%s': No such file or directory\n", only, dir_path);
+						if (ave[index + 1] == NULL)
 							free(buf), free(only), exit(1);
 					}
 					if (dir)
