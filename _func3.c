fbc89672c6d43ac78d5776c76341ccb2b15fd913
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-16 16:07:51 +0300

    update _func3 with betty style

--- _func3.c
+++ _func3.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
+ * take_only_cmd - Extract the first command from a string
+ * @buffer: Input string containing one or more commands
  *
- *
- *
-*/
-
+ * Return: Pointer to the first command
+ */
 char *take_only_cmd(char *buffer)
 {
-        int i = 0;
-        char str_cmd[50], *cmd;
+	int i = 0;
+	char str_cmd[50], *cmd;
 
 	if (_strlen(buffer) >= 50)
 	{
@@ -17,73 +17,69 @@ char *take_only_cmd(char *buffer)
 		free(buffer), exit(1);
 	}
 	while (buffer[i])
-        {
-                if (buffer[i] == ' ')
-                        break;
-                str_cmd[i] = buffer[i];
-                i++;
-        }
-        str_cmd[i] = '\0';
-        cmd = malloc(_strlen(str_cmd) + 1);
-        if (!cmd)
-        {
-                perror("fail to allocate");
-                exit(1);
-        }
-        strcpy(cmd, str_cmd);
-	/*if (_strcmp(cmd, "cd") == 0)
 	{
-		write(1, "cd Y", 4);
-		buffer = NULL;
-	}*/
+		if (buffer[i] == ' ')
+			break;
+		str_cmd[i] = buffer[i];
+		i++;
+	}
+	str_cmd[i] = '\0';
+	cmd = malloc(_strlen(str_cmd) + 1);
+	if (!cmd)
+	{
+		perror("fail to allocate");
+		exit(1);
+	}
+	strcpy(cmd, str_cmd);
+
 	return (cmd);
 }
 
 /**
+ * _getline - Read a line from a file stream
+ * @line: Pointer to a pointer that will store the read line
+ * @n: Pointer to the size of the buffer allocated for the line
+ * @stream: File stream to read from
  *
- *
-*/
+ * Return: Number of bytes read
+ */
 ssize_t _getline(char **line, size_t *n, FILE *stream)
 {
+	size_t size_buffer = *n, total_byt_read = 0;
+	ssize_t num_of_byt_reads;
+	char one_byte, *new_size_ptr;
 
-    size_t size_buffer = *n, total_byt_read = 0;
-    ssize_t num_of_byt_reads;
-    char one_byte, *new_size_ptr;
+	if (line == NULL || n == NULL || stream == NULL)
+		return (-1);
+	if (*line == NULL)
+	{
+		size_buffer = 128;
+		*line = malloc(size_buffer);
+		if (*line == NULL)
+			return (-1);
+		*n = size_buffer;
+	}
+	while ((num_of_byt_reads = read(fileno(stream), &one_byte, 1)) > 0)
+	{
+		if (total_byt_read >= size_buffer - 1)
+		{
+			size_buffer += 128;
+			new_size_ptr = realloc(*line, size_buffer);
+			if (!new_size_ptr)
+				return (-1);
+			*n = size_buffer;
+			*line = new_size_ptr;
+		}
+		(*line)[total_byt_read++] = one_byte;
+		if (one_byte == '\n')
+			break;
+	}
 
-    if (line == NULL || n == NULL || stream == NULL)
-        return -1;
+	if (total_byt_read == 0 && num_of_byt_reads <= 0)
+	{
+		return (-1);
+	}
 
-    if (*line == NULL)
-    {
-        size_buffer = 128;
-        *line = malloc(size_buffer);
-        if (*line == NULL)
-            return -1;
-        *n = size_buffer;
-    }
-
-    while ((num_of_byt_reads = read(fileno(stream), &one_byte, 1)) > 0)
-    {
-        if (total_byt_read >= size_buffer - 1)
-        {
-            size_buffer += 128;
-            new_size_ptr = realloc(*line, size_buffer);
-            if (!new_size_ptr)
-                return -1;
-            *n = size_buffer;
-            *line = new_size_ptr;
-        }
-
-        (*line)[total_byt_read++] = one_byte;
-
-        if (one_byte == '\n')
-            break;
-    }
-
-    if (total_byt_read == 0 && num_of_byt_reads <= 0)
-        return -1;
-
-    (*line)[total_byt_read] = '\0';
-
-    return total_byt_read;
+	(*line)[total_byt_read] = '\0';
+	return (total_byt_read);
 }
