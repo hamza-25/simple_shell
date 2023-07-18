32fc9e27d1b3983c6810e40af2138b4b6c1ab851
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-18 18:15:17 +0300

    update _func4.c with betty style

--- _func4.c
+++ _func4.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
+ * change_dir - Changes the current working directory
  *
- *
- * const char *path
-*/
+ * @buffer: The input command buffer
+ * @cmd: The command name
+ */
 void change_dir(char *buffer, char *cmd)
 {
 	char *token, *back = NULL, *now = NULL;
@@ -12,26 +14,26 @@ void change_dir(char *buffer, char *cmd)
 	back = getenv("OLDPWD");
 	token = strtok(buffer, " ");
 	token = strtok(NULL, " ");
-	if (token == NULL) 
+	if (token == NULL)
 	{
-        	if (chdir(getenv("HOME")) != 0)
-            		perror("cd"), free(buffer), free(cmd), exit(1);
-        	setenv("OLDPWD", now, 1);
-    	} 
-	else if (_strcmp(token, "-") == 0)
-       	{
-       		 if (back == NULL)
-            		write(2, "cd: OLDPWD not set\n", _strlen("cd: OLDPWD not set\n"));
-        	if (chdir(back) != 0)
+		if (chdir(getenv("HOME")) != 0)
 			perror("cd"), free(buffer), free(cmd), exit(1);
-        	setenv("OLDPWD", now, 1);
-        	_printf("%s\n", back);
+		setenv("OLDPWD", now, 1);
+	}
+	else if (_strcmp(token, "-") == 0)
+	{
+		if (back == NULL)
+			write(2, "cd: OLDPWD not set\n", _strlen("cd: OLDPWD not set\n"));
+			if (chdir(back) != 0)
+				perror("cd"), free(buffer), free(cmd), exit(1);
+			setenv("OLDPWD", now, 1);
+			_printf("%s\n", back);
 	}
 	else
 	{
-
-		if (chdir(token) != 0)
-            		_printf("-bash: %s: %s: No such file or directory\n", cmd, token), free(buffer), free(cmd);
-        	setenv("OLDPWD", now, 1);
+	if (chdir(token) != 0)
+		_printf("-bash: %s: %s: No such file or directory\n", cmd, token),
+		free(buffer), free(cmd);
+	setenv("OLDPWD", now, 1);
 	}
 }
