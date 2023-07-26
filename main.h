dcb361433e3d2f73854298da6874ccc3ddd15ec9
hamza-25 <me677499@gmail.com>
2023-07-26 00:14:06 +0000

    fix betty style

--- main.h
+++ main.h
@@ -22,7 +22,8 @@ int main(int argc, char *argv[], char *env[]);
 size_t _strlen(const char *str);
 void _print(const char *str);
 char *_strdup(const char *str);
-void _fork(int argc, char *argv[], char *buf, char **ave, char *only, int *status);
+void _fork(int argc, char *argv[], char *buf,
+		char **ave, char *only, int *status);
 int _strcmp(const char *string1, const char *string2);
 void tok_buf(char *buf, char *args[], const char *del,
 							char *str, char *env[]);
@@ -30,17 +31,22 @@ char *full_path(char *env[], char cmd[]);
 void _printf(const char *format, ...);
 void tostring(int num);
 char *path_check(char path[]);
-void _env();
-void non_interactive(int argc, char *argv[], char *env[], int *pipe);
-char *take_only_cmd(char **buffer, int *no_exc, int argc, char *argv[], int *n_err);
+void _env(void);
+void non_interactive(int argc, char *argv[],
+		char *env[], int *pipe);
+char *take_only_cmd(char **buffer, int *no_exc,
+		int argc, char *argv[], int *n_err);
 ssize_t _getline(char **line, size_t *n, FILE *stream);
 void ls_check(char *ave[], char *buf, char *only);
 void change_dir(char *buffer, char *cmd, int argc, char *argv[], int *n_err);
 void comments(char **buf, int *no_exc);
-void tok(char **buf, const char **del, char **token, char **str, char *args[]);
-void handle_input_command(char **buffer, size_t *n_buffer, int *no_exc, char **only_command, int status, int argc, char *argv[],int *n_err);
+void tok(char **buf, const char **del, char **token,
+		char **str, char *args[]);
+void handle_input_command(char **buffer, size_t *n_buffer,
+		int *no_exc, char **only_command, int status,
+		int argc, char *argv[], int *n_err);
 void handle_sigint(int sig_num);
-char *name_prg(int argc , char *argv[]);
+char *name_prg(int argc, char *argv[]);
 void shell_exit(int status);
 void _env_non(int *no_exc);
 
